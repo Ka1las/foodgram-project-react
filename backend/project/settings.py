ef40cca7efd9587ab59eae4a40264f191825d44d
Антон Михайлов <insept17@yandex.ru>
2022-09-15 06:11:00 +0600

    10

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='3#$0643aiqiyt91a3g*unq1p@y_h$y(qy72z0xh+-z$s+0&h=7')
 
-DEBUG = os.getenv('DEBUG', default='False')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
